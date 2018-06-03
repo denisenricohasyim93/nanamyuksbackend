4a45d759cc6fe3f0ec40c97917e59467f66cf27c
denisenricohasyim93 <denisenricohasyim93@gmail.com>
2018-06-03 08:37:02 +0700

    another commit

--- routes/pots.js
+++ routes/pots.js
@@ -3,7 +3,7 @@ var app = express()
 
 app.get('/', function(req, res, next) {
     req.getConnection(function(error, conn) {
-        conn.query('SELECT * FROM pots ORDER BY id DESC',function(err, rows, fields) {
+        conn.query('SELECT * FROM pots ORDER BY pot_id DESC',function(err, rows, fields) {
             if (error) throw error;
             return res.send({ error: false, data: rows, message: 'Pots data successfully retrieved' });
         })
@@ -15,10 +15,12 @@ app.post('/add', function(req,res,next){
         let soil_sensor_value = req.body.soil_sensor_value;
         let temperature = req.body.temperature;
         let light_intensity = req.body.light_intensity;
+        let user_id = req.body.user_id;
         let pot = {
             soil_sensor_value: soil_sensor_value,
             temperature: temperature,
-            light_intensity: light_intensity
+            light_intensity: light_intensity,
+            user_id: user_id
         }    
         conn.query("INSERT INTO pots SET ? ", pot, function (error, results, fields) {
             if (error) throw error;
@@ -29,16 +31,18 @@ app.post('/add', function(req,res,next){
 
 app.put('/edit', function(req, res, next) {
     req.getConnection(function(error, conn) {
-        let id = req.body.id;
+        let pot_id = req.body.pot_id;
         let soil_sensor_value = req.body.soil_sensor_value;
         let temperature = req.body.temperature;
         let light_intensity = req.body.light_intensity;
+        let user_id = req.body.user_id;
         let pot = {
             soil_sensor_value: soil_sensor_value,
             temperature: temperature,
-            light_intensity: light_intensity
+            light_intensity: light_intensity,
+            user_id: user_id
         }  
-        conn.query('UPDATE pots SET ? WHERE id = ' + id, pot, function(err, result) {
+        conn.query('UPDATE pots SET ? WHERE pot_id = ' + pot_id, pot, function(err, result) {
             if (error) throw error;
             return res.send({ error: false, data: result, message: 'Pot successfully updated' });
         })
@@ -47,12 +51,12 @@ app.put('/edit', function(req, res, next) {
 
 app.delete('/delete', function (req, res) {
     req.getConnection(function(error, conn) {
-        let id = req.body.id;
+        let pot_id = req.body.pot_id;
     
         if (!id) {
             return res.status(400).send({ error: true, message: 'Please provide id' });
         }
-        conn.query('DELETE FROM pots WHERE id = ?', [id], function (error, results, fields) {
+        conn.query('DELETE FROM pots WHERE pot_id = ?', [pot_id], function (error, results, fields) {
             if (error) throw error;
             return res.send({ error: false, data: results, message: 'Pot has been deleted successfully.' });
         });
